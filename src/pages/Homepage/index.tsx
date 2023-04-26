a54a26b27d4d7a998ec23f315ce89a79d9bb1237
vgabrielk <vfelix1553@gmail.com>
2023-04-26 12:33:52 -0300

    feat: Interface homepage finalizada✅

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,22 @@
+import './index.scss'
+
+const Homepage = () => {
+  return (
+    <main className='home'>
+        <div className="logo">
+            <img width="100px" src="/src/assets/svg/logo.svg" alt="" />
+        </div>
+        <span className='texts'>
+            <h1>Alcance mais clientes</h1>
+            <p>Se você precisa de um site de alta performance para aumentar suas vendas, você veio ao lugar certo.</p>
+        </span>
+        <button className="button">
+            <p>Meu contato</p>
+            <img src="/src/assets/svg/arrow.svg" alt="" />
+        </button>
+        <img src="/src/assets/svg/yellow-blur.svg" alt="" className="light-blur" />
+    </main>
+  )
+}
+
+export default Homepage
